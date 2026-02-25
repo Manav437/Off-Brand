d7d35591fd0a7a28f013bc82d9c5724785df445b
Manav Gusain <manav.gussain@gmail.com>
2026-02-25 21:10:26 +0530

    type fixes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,8 +1,8 @@
-import { motion } from "framer-motion";
+import { motion, type Variants } from "framer-motion";
 import bgImg from "../assets/hero-section-img.jpeg";
 
 const HeroSection = () => {
-    const titleVariant = {
+    const titleVariant: Variants = {
         hidden: { y: "103%" },
         visible: {
             y: 0,
@@ -13,7 +13,7 @@ const HeroSection = () => {
         },
     };
 
-    const containerVariant = {
+    const containerVariant: Variants = {
         hidden: { opacity: 0 },
         visible: {
             opacity: 1,
